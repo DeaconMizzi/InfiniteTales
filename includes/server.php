87b651a44d1dac9bb67d6409312bc5fb3a14d8c0
Deacon <deaconmizzi@gmail.com>
2024-01-11 18:09:56 +0100

    Login, Sign Up, Admin Page
    (Changed PC midway through coding so had to move everything), Code is the Login and signup functions, along with adding / deleting books in the admin page.

---
+++ includes/server.php
@@ -0,0 +1,210 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$house    = "";
+$street   = "";
+$code     = "";
+$town     = "";
+$title    = "";
+$author   = "";
+$category = "";
+$quantity = "";
+$price    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'infinitetales');
+
+if (isset($_POST['add_product']))
+{
+    $title = mysqli_real_escape_string($db, $_POST['title']);
+    $author = mysqli_real_escape_string($db, $_POST['author']);
+    $category = mysqli_real_escape_string($db, $_POST['category']);
+    $quantity = mysqli_real_escape_string($db, $_POST['quantity']);
+    $price = mysqli_real_escape_string($db, $_POST['price']);
+
+    $book_check_query = "SELECT * FROM book WHERE title='$title' LIMIT 1";
+    $result = mysqli_query($db, $book_check_query);
+    $book = mysqli_fetch_assoc($result);
+  
+    if ($book) { // if book exists
+        if ($book['title'] === $title) {
+            array_push($errors, "Book already exists");
+        }
+
+    }
+
+    if (count($errors) == 0) {
+        $query = "INSERT INTO book (title, author, category, quantity, price) 
+                  VALUES('$title', '$author', '$category', '$quantity', '$price')";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "Book added to database";
+        header('location: admin.php');
+    }
+}
+
+if (isset($_POST['delete_book'])) {
+    $book_id_to_delete = $_POST['book_id'];
+    $delete_query = "DELETE FROM book WHERE bookId = '$book_id_to_delete'";
+    if (mysqli_query($db, $delete_query)) {
+        echo "<script>alert('Record deleted successfully');</script>";
+        header("Refresh:0"); // Refresh the page after deletion
+        exit();
+    } else {
+        echo "Error deleting record: " . mysqli_error($db);
+    }
+}
+
+$sql = "SELECT * FROM book";
+$result = mysqli_query($db, $sql);
+
+// Display Book Table
+function displayBooks($db) {
+    // Fetch data from the book table
+    $sql = "SELECT * FROM book";
+    $result = mysqli_query($db, $sql);
+
+    // Display Book Table
+    if (mysqli_num_rows($result) > 0) {
+        // Output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<th scope='row'><input type='checkbox' /></th>";
+            echo "<td class='tm-product-name'>" . $row['title'] . "</td>";
+            echo "<td>" . $row['author'] . "</td>";
+            echo "<td>" . $row['category'] . "</td>";
+            echo "<td>" . $row['quantity'] . "</td>";
+            echo "<td>" . $row['price'] . "</td>";
+            echo "<td>";
+            echo "<form method='POST' action='admin.php'>";
+            echo "<input type='hidden' name='book_id' value='" . $row['bookId'] . "'>";
+            echo "<button class='btn btn-primary btn-block text-uppercase mb-3' name='delete_book'>Delete</button>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "0 results";
+    }
+}
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+    // receive all input values from the form
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled ...
+    // by adding (array_push()) corresponding error unto $errors array
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($password_1)) { array_push($errors, "Password is required"); }
+    if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    // first check the database to make sure 
+    // a user does not already exist with the same username and/or email
+    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+  
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($user['email'] === $email) {
+            array_push($errors, "email already exists");
+        }
+    }
+
+    // Finally, register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = $password_1;
+
+        $query = "INSERT INTO users (username, email, password) 
+                  VALUES('$username', '$email', '$password')";
+        mysqli_query($db, $query);
+        $_SESSION['success'] = "You are now logged in";
+        header('location: index.php');
+    }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    if (count($errors) == 0) {
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            $user = mysqli_fetch_assoc($results);
+            $_SESSION['user_id'] = $user['accountId'];
+            header('location: index.php');
+        } else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+}
+
+if (isset($_POST['update_profile'])) {
+  $user_id = $_SESSION['user_id'];
+  $house = mysqli_real_escape_string($db, $_POST['houseNum']);
+  $street = mysqli_real_escape_string($db, $_POST['street']);
+  $code = mysqli_real_escape_string($db, $_POST['postCode']);
+  $town = mysqli_real_escape_string($db, $_POST['town']);
+
+  if (count($errors) == 0) {
+      // Check if the user already has an address
+      $address_query = "SELECT * FROM address INNER JOIN users ON address.addressId = users.addressId WHERE users.accountId = $user_id";
+      $address_result = mysqli_query($db, $address_query);
+      $existing_address = mysqli_fetch_assoc($address_result);
+
+      if ($existing_address) {
+          $addressId = $existing_address['addressId'];
+          $update_address_query = "UPDATE address SET houseNumber='$house', street='$street', postCode='$code', town='$town' WHERE addressId=$addressId";
+
+          if (mysqli_query($db, $update_address_query)) {
+              $_SESSION['success'] = "Address updated successfully";
+              header('location: index.php');
+              exit();
+          } else {
+              echo "Error updating address: " . mysqli_error($db);
+          }
+      } else {
+          $insert_address_query = "INSERT INTO address (houseNumber, street, postCode, town) VALUES ('$house', '$street', '$code', '$town')";
+          if (mysqli_query($db, $insert_address_query)) {
+              $addressId = mysqli_insert_id($db);
+
+              $update_user_query = "UPDATE users SET addressId = $addressId WHERE accountId = $user_id";
+              if (mysqli_query($db, $update_user_query)) {
+                  $_SESSION['success'] = "Address added successfully";
+                  header('location: user.php');
+                  exit();
+              } else {
+                  echo "Error updating user with address: " . mysqli_error($db);
+              }
+          } else {
+              echo "Error inserting address: " . mysqli_error($db);
+          }
+      }
+  }
+}
+?>
--- signup.php
+++ signup.php
@@ -1,35 +1,35 @@
+
 <?php
     include 'includes/header.php';
 ?>
 
 <div class="form-container">
-<form action="/action_page.php" style="border:1px solid #ccc;" class="formSign">
-  <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+  <form method = "post" action="signup.php" style="border:1px solid #ccc;" class="formSign">
+    <?php include('errors.php'); ?>
+    <div class="container">
+      <h1>Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+      <label for="email"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" value="<?php echo $username; ?>" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" value="<?php echo $email; ?>" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-    
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-    
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password_1" required>
 
-    <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="password_2" required>
+      
+
+      <div class="clearfix">
+        <button type="submit" class="signupbtn" name="reg_user">Sign Up</button>
+      </div>
+        Already a member? <a href="signin.php">Sign in</a>
     </div>
-  </div>
-</form>
+  </form>
 </div>
 <?php
     include 'includes/footer.php';
